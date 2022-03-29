7659c7ed4ec27ec64c2e4c1605854aad0982b6ef
eduardohenrique <eduardo@joycar.com.br>
2022-03-29 20:23:36 -0300

    Ajustes em stopwords e geração de csvs

--- code/main.py
+++ code/main.py
@@ -3,5 +3,4 @@ from util import *
 df = generate_data_frame()
 df = df.drop_duplicates(subset=["text"])
 
-
-generate_word_cloud(df)
+show_word_cloud(df)
--- code/util.py
+++ code/util.py
@@ -6,7 +6,9 @@ import tweepy
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 from dotenv import load_dotenv
-from ntlk.corpus import stopwords
+
+from nltk.corpus import stopwords
+
 stop_words = stopwords.words('portuguese')
 
 load_dotenv()
@@ -14,6 +16,7 @@ load_dotenv()
 TOKEN: str = os.getenv('TOKEN')
 
 client: tweepy.Client = tweepy.Client(bearer_token=TOKEN)
+
 
 def get_data(query: str, end_time: datetime, *args) -> tweepy.client.Response:
     tweets: tweepy.client.Response = client.search_recent_tweets(query=query,
@@ -29,17 +32,19 @@ def get_data(query: str, end_time: datetime, *args) -> tweepy.client.Response:
     return tweets
 
 
-def generate_word_cloud(df: pd.DataFrame):
+def show_word_cloud(df: pd.DataFrame):
     text = " ".join(item.split()[1] for item in df["text"])
-    wordcloud = WordCloud(width=1000, height=1000,
+    wordcloud = WordCloud(width=2000, height=2000,
                           background_color='white',
-                          stopwords=stopwords,
-                          min_font_size=8).generate(text)
+                          stopwords=stop_words,
+                          min_font_size=14).generate(text)
 
     plt.figure(figsize=(8, 8), facecolor=None)
     plt.imshow(wordcloud)
+    plt.axis("off")
     plt.tight_layout(pad=0)
     plt.show()
+
 
 def generate_data_frame() -> pd.DataFrame:
     end_time: datetime = datetime.now()
@@ -47,7 +52,7 @@ def generate_data_frame() -> pd.DataFrame:
     data: list[dict] = []
     response: tweepy.client.Response = get_data(query, end_time)
     users: list = response.includes['users']
-    limit: int = 20000
+    limit: int = 10000
 
     while len(data) <= limit:
         for user, tweet in zip(users, response.data):
@@ -69,4 +74,5 @@ def generate_data_frame() -> pd.DataFrame:
             break
 
     df = pd.DataFrame(data)
+    df.to_csv(f"csvs/{0}_{1}.csv", query.split(" ")[0], str(end_time))
     return df
